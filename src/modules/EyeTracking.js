d811e100be38942a3b140fc5530b1b4e481fa52c
Sekiro4321 <155538724+Sekiro4321@users.noreply.github.com>
2026-04-20 14:26:31 +0530

    Working model

--- src/modules/EyeTracking.js
+++ src/modules/EyeTracking.js
@@ -69,7 +69,7 @@ export class EyeTracking {
       this.faceLandmarker = await FaceLandmarker.createFromOptions(vision, {
         baseOptions: {
           modelAssetPath:
-            'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/face_landmarker.task',
+            'https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task',
           delegateOptions: {
             delegate: 'GPU'
           }
